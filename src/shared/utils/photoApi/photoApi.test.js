b39f4c87d655e60aa43b5c03b6528ef2348c4750
kacper-dabrowski <kdtw301@gmail.com>
2021-02-19 16:13:41 +0100

    Tests - Add tests for photoApi util

--- src/shared/utils/photoApi/photoApi.test.js
+++ src/shared/utils/photoApi/photoApi.test.js
@@ -1,3 +1,32 @@
-it('should pass', () => {
-    expect(true).toBe(true);
+import websources from '../../websources';
+import getPhotoData from './photoApi';
+import axios from 'axios';
+
+jest.mock('axios');
+
+it('should return a valid photo url', async () => {
+    jest.spyOn(axios, 'post').mockImplementation(async () => withValidPhotoApiResponse());
+    const text = 'DUMMY_TEXT';
+    const options = 'DUMMY_OPTIONS';
+
+    const result = await getPhotoData(text, options);
+
+    expect(result).toBe(`data:image/png;base64,PHOTO_URL`);
 });
+
+it('should throw an error, if response status is not 200', async () => {
+    jest.spyOn(axios, 'post').mockImplementation(async () => withInvalidPhotoApiResponse());
+
+    const text = 'DUMMY_TEXT';
+    const options = 'DUMMY_OPTIONS';
+
+    await expect(getPhotoData(text, options)).rejects.toEqual(new Error('Photo request failed'));
+});
+
+function withValidPhotoApiResponse() {
+    return { data: { photo: 'PHOTO_URL' }, status: 200 };
+}
+
+function withInvalidPhotoApiResponse() {
+    return { data: { photo: 'PHOTO_URL' }, status: 400 };
+}
