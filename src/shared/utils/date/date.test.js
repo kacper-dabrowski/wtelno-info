de5106e2e8add33176637a2ec93c28eeb7f35cec
kacper-dabrowski <kdtw301@gmail.com>
2021-02-19 16:39:57 +0100

    Tests - Add format date tests

---
+++ src/shared/utils/date/date.test.js
@@ -0,0 +1,17 @@
+import { formatDate } from './date';
+
+it('should return a valid formatted date', () => {
+    const date = 'Fri Feb 19 2021 16:23:11 GMT+0100 (Central European Standard Time)';
+
+    const formattedDate = formatDate(date);
+
+    expect(formattedDate).toEqual('19-02-2021 16:23');
+});
+
+it('should return an empty string if invalid date was passed', () => {
+    const date = 'DUMMY_DATE';
+
+    const formattedDate = formatDate(date);
+
+    expect(formattedDate).toEqual('');
+});
