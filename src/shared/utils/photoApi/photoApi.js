1befe10188c0f6b69c1679be93e68b3402f449f9
kacper-dabrowski <kdtw301@gmail.com>
2021-02-19 16:00:51 +0100

    Testing - Add tests setup

--- src/shared/utils/photoApi/photoApi.js
+++ src/shared/utils/photoApi/photoApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import websources from '../websources';
+import websources from '../../websources';
 
 const getPhotoData = async (text, options) => {
     const response = await axios.post(websources.PHOTOS_API_URL, { text, options });
